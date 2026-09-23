89167a257cd94e848e630e1a6fc073b8cb7a214a
Doreen000 <doreenlin911@gmail.com>
2026-09-23 16:03:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('myIDForToClick').onclick = function() {
+    alert("Nice Click!");
+}
+
+function clicked() {
+    document.getElementById('title').style.color = "red";
+}
+
+function clicked2() {
+    document.getElementById('title').style.color = "black";
+}
